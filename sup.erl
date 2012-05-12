0e537d6b54e50d65846b3113b4852c4926a0ba3d
Juraj Hlista <juro.hlista@gmail.com>
2012-05-12 16:25:53 +0200

    added supervisor template

---
+++ sup.erl
@@ -0,0 +1,44 @@
+%%%-------------------------------------------------------------------
+%%% @author Juraj Hlista <juro.hlista@gmail.com>
+%%% @copyright
+%%% @doc
+%%% supervisor template
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(sup).
+
+-behaviour(supervisor).
+
+%% Public API
+-export([
+		 start_link/0
+		]).
+
+%% supervisor callbacks
+-export([
+		 init/1
+		]).
+
+-define(SERVER, ?MODULE).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+%% @doc Starts supervisor
+start_link() ->
+	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+%% @private
+%% @doc
+init([]) ->
+	ChildProcess = {child_process, {child_process, start_link, []},
+		permanent, 5000, worker, [child_process]},
+	Children = [ChildProcess],
+	RestartStrategy = {one_for_one, 5, 10},
+	{ok, {RestartStrategy, Children}}.
